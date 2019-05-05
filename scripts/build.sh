9b2626849a8c6b0cc7a31f87b3eed69907456b5b
yuriy1999 <yurii.c@nerdysoft.com>
2019-05-05 16:33:23 +0300

    _Add deploy-test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# docker build -t testherokuimg . 
-# docker tag testherokuimg:latest registry.heroku.com/limitless-forest-11326/web
-# docker push registry.heroku.com/limitless-forest-11326/web
-
-echo "docker username $DOCKER_USERNAME"
-echo "docer pass $DOCKER_PASSWORD"
-echo "heroku app name $HEROKU_APP_NAME"
+docker build -t testherokuimg . 
+docker tag testherokuimg:latest registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
---
+++ scripts/deploy.sh
@@ -0,0 +1,1 @@
+heroku container:web release --app $HEROKU_APP_NAME

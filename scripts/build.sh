20c5d87d219340f086982c4954ff20228cc268f4
yuriy1999 <yurii.c@nerdysoft.com>
2019-05-06 13:23:51 +0300

    Add condition to deploy-test stage

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
 docker build -t testherokuimg . 
 docker tag testherokuimg:latest registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web
